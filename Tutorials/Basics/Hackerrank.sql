28f1dc99f3b3beb300652ed102c009428364ecca
Rohit Karmudi <rohitkarmudi1996@gmail.com>
2025-03-12 01:48:08 +0000

    Initial Commit

---
+++ Tutorials/Basics/Hackerrank.sql
@@ -0,0 +1,5 @@
+1. Query the following two values from the **STATION** table:
+    1. The sum of all values in *LAT_N* rounded to a scale of  decimal places.
+    2. The sum of all values in *LONG_W* rounded to a scale of  decimal places.
+
+    SELECT ROUND(SUM(LAT_N), 2), ROUND(SUM(LONG_W),2) FROM STATION
